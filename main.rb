4691a8ee800d0774e927b42867e7ce43a4d5c7ae
cyee <you@example.com>
2013-03-27 15:14:08 +0900

    Some changes

--- main.rb
+++ main.rb
@@ -1,17 +1,19 @@
+# encoding: UTF-8
+
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/streaming'
-require "sinatra/reloader" if development?
+require 'sinatra/reloader' if development?
+require './library'
+require './tokenizer'
 
 get '/' do
   stream do |out|
     me = EnglishPhrase.first
-    out << "#{me.original}\n"
-    sleep 5
-    out << "#{EnglishPhrase.last.original}\n"
-   out << 'huuuwwwwe'
+    file = Tokenizer::Parser.new 'data.txt', 'CP950'
+    file.each do |line|
+      out << "#{Tokenizer.tokenize(line)}\n"
+    end
   end
 end
 
-class EnglishPhrase < ActiveRecord::Base
-end
