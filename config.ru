fc580815c1f3135ffbb6056964d7c9de528bee6f
cyee <you@example.com>
2013-03-29 15:52:02 +0900

    Saved

--- config.ru
+++ config.ru
@@ -4,3 +4,9 @@ run Sinatra::Application
 set :environment, :development
 set :server, :thin
 set :database, ENV['DATABASE_URL'] || 'postgres://localhost/mt1'
+set :public_folder, Proc.new { File.join(root, "static") }
+set :haml, :format => :html5
+
+error do
+  'Sorry there was a nasty error - ' + env['sinatra.error'].name
+end
--- main.rb
+++ main.rb
@@ -1,19 +1,35 @@
 # encoding: UTF-8
 
+require 'pry'
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/streaming'
 require 'sinatra/reloader' if development?
+require 'haml'
 require './library'
 require './tokenizer'
 
-get '/' do
+get '/parse' do
+  haml :layout
   stream do |out|
-    me = EnglishPhrase.first
-    file = Tokenizer::Parser.new 'data.txt', 'CP950'
-    file.each do |line|
-      out << "#{Tokenizer.tokenize(line)}\n"
-    end
+    $out = out #debug
+    kheperize 'data'
   end
 end
 
+def kheperize(folder)
+  files = Dir.entries(folder) - %w(. .. .DS_Store)  #skip . .. files
+  files.each do |path|
+    binding.pry
+    file = Tokenizer::Parser.new "#{folder}/#{path}", external_encoding:'utf-8'
+    file.take(10).each do |line|
+    binding.pry
+      # for each line
+      words = Tokenizer.tokenize(line).unshift('<s>')
+      Tokenizer.process(words)
+    end
+  end
+  #f1.each.zip(f2.each).each do |line1, line2|
+      # Do something with the lines
+    # end
+end
